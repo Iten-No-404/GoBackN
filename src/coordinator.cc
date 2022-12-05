b5a0d7ecfb59f7c19458e4454a6a541c4202d517
Iten Elhak <iten_emad@yahoo.com>
2022-12-05 20:38:55 +0200

    Project Starter + Basic Features
    - Network components: Coordinator & Node.
    - Initialization File Parameters.
    - Reading Input from text files.
    - Framing Algorithm using Byte Stuffing.
    
    Co-Authored-By: Radwa Ahmed <56734728+radwaahmed2132000@users.noreply.github.com>

---
+++ src/coordinator.cc
@@ -0,0 +1,76 @@
+/*
+ * cooridinator.cc
+ *
+ *  Created on: Dec 4, 2022
+ *
+ */
+
+#include <string>
+#include <omnetpp.h>
+#include <fstream>
+
+using namespace omnetpp;
+
+/**
+ *
+ */
+class Coordinator : public cSimpleModule
+{
+  protected:
+    // The following redefined virtual function holds the algorithm.
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
+    char readInputFile(const char *filename);
+};
+
+// The module class needs to be registered with OMNeT++
+Define_Module(Coordinator);
+
+void Coordinator::initialize()
+{
+    cMessage *senderMsg = new cMessage("Yes");
+    cMessage *receiverMsg = new cMessage("No");
+    std::ifstream OpenFile;
+    char sender = '0';
+    char receiver = '1';
+    char content = readInputFile("coordinator.txt");
+    std::string string1 = "port0$o";
+    std::string string2 = "port1$o";
+    if(content == '0'){
+        send(senderMsg, string1.c_str());
+        send(receiverMsg, string2.c_str());
+    } else{
+        send(senderMsg, string2.c_str());
+        send(receiverMsg, string1.c_str());
+    }
+
+}
+
+char Coordinator::readInputFile(const char *filename)
+{
+    std::ifstream filestream;
+    std::string line;
+
+    filestream.open(filename, std::ifstream::in);
+
+    if(!filestream) {
+        throw cRuntimeError("Error opening file '%s'?", filename);
+        return -1;
+    } else {
+        while ( getline(filestream, line) ) {
+            if (line.find('#') == 0) {
+                continue; // ignore comment lines
+            }
+            else {
+                EV<<line;
+                return line[line.size()-1];
+            }
+        }
+    }
+    return '_';
+}
+
+void Coordinator::handleMessage(cMessage *msg)
+{
+
+}
---
+++ src/node.cc
@@ -0,0 +1,119 @@
+/*
+ * node.cc
+ *
+ *  Created on: Dec 4, 2022
+ *
+ */
+#include <string>
+#include <omnetpp.h>
+#include <fstream>
+#include <vector>
+
+#define flag '$'
+#define escape '/'
+using namespace omnetpp;
+
+/**
+ * Derive the Node class from cSimpleModule. This is the class where a message is sent as a reply to each
+ * message received from the hub.
+ */
+class Node : public cSimpleModule
+{
+  protected:
+    // data members
+    bool sender = false;
+    bool initial = true;
+    int index = 0;
+    int seqNum = 0;
+    std::vector<std::string> errors,messages;
+    // The following redefined virtual function holds the algorithm.
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
+    void readInputFile(const char *filename);
+    std::string byteStuffing();
+};
+
+// The module class needs to be registered with OMNeT++
+Define_Module(Node);
+
+void Node::initialize()
+{
+}
+
+void Node::handleMessage(cMessage *msg)
+{
+    EV<<msg->getFullName();
+    std::string sending ="Yes";
+    if(initial && msg->getFullName() == sending){
+        EV<<"Entered!";
+        sender = true;
+        if(isName("node0"))
+            index = 0;
+        else
+            index = 1;
+        std::string fileName = "input"+std::to_string(index)+".txt";
+        readInputFile(fileName.c_str());
+        initial = false;
+    } else if(initial) {
+        initial = false;
+        return;
+    }
+    if(sender){
+        if(seqNum<messages.size()){
+            std::string value = byteStuffing();
+            cMessage *newMsg = new cMessage(" ");
+            newMsg->setName(value.c_str());
+            send(newMsg, "nodeGate$o"); // send out the message
+            seqNum++;
+        }
+    } else {
+        cMessage *ackMsg = new cMessage("ACK");
+        send(ackMsg, "nodeGate$o"); // send out the message
+    }
+}
+
+void Node::readInputFile(const char *filename)
+{
+    std::ifstream filestream;
+    std::string line;
+
+    filestream.open(filename, std::ifstream::in);
+
+    if(!filestream) {
+        throw cRuntimeError("Error opening file '%s'?", filename);
+    } else {
+        while ( getline(filestream, line) ) {
+            if (line.find('#') == 0) {
+                continue; // ignore comment lines
+            }
+            else {
+//                EV<<line;
+//                EV<<"\n";
+                std::string err = line.substr(0,4);
+                errors.push_back(err);
+                std::string mes = line.substr(5);
+                messages.push_back(mes);
+//                EV<<err;
+//                EV<<"\n";
+//                EV<<mes;
+//                EV<<"\n";
+//                return line[line.size()-1];
+            }
+        }
+    }
+    return;
+}
+
+std::string Node::byteStuffing(){
+    std::string s = "$";
+    for(int i=0; i<messages[seqNum].size(); i++)
+    {
+        if(messages[seqNum][i] == flag || messages[seqNum][i] == escape)
+        {
+            s += escape;
+        }
+        s += messages[seqNum][i];
+    }
+    s += flag;
+    return s;
+}
